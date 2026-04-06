a57af7d7486dc3a93bea90a1f9881424576ae6a3
Юрий Кудрин <rexwolf163@gmail.com>
2026-04-07 02:30:44 +0400

    расширение функционала таймера

--- Sdk/MiniGamesSystem/MiniGames/Handlers/GameTimerView.cs
+++ Sdk/MiniGamesSystem/MiniGames/Handlers/GameTimerView.cs
@@ -1,9 +1,11 @@
-﻿using Vortex.Sdk.MiniGamesSystem.MiniGames.Abstractions;
+﻿using System;
+using Vortex.Sdk.MiniGamesSystem.MiniGames.Abstractions;
 using Vortex.Sdk.MiniGamesSystem.MiniGames.Model;
 using UnityEngine;
 using UnityEngine.UI;
 using Vortex.Core.System.Abstractions;
 using Vortex.Unity.EditorTools.Attributes;
+using Vortex.Unity.UI.StateSwitcher;
 
 namespace Vortex.Sdk.MiniGamesSystem.MiniGames.Handlers
 {
@@ -11,6 +13,8 @@ namespace Vortex.Sdk.MiniGamesSystem.MiniGames.Handlers
     {
         [SerializeField, ClassFilter(typeof(IDataStorage))]
         private MonoBehaviour source;
+
+        [SerializeField] private bool reverse;
 
         private IDataStorage _storage;
         private IDataStorage Storage => _storage ??= source as IDataStorage;
@@ -20,8 +24,25 @@ namespace Vortex.Sdk.MiniGamesSystem.MiniGames.Handlers
         private MiniGameData _data;
         private IGameModelWithTimer _dataTimer;
 
+        [SerializeField] private UIStateSwitcher stageSwitcher;
+
+        private float _step;
+        private int _currentStage;
+        private int _stages;
+
         private void OnEnable()
         {
+            slider.maxValue = 1f;
+            slider.minValue = 0f;
+            _stages = stageSwitcher?.States.Length - 1 ?? 0;
+            _currentStage = 0;
+            _step = 0;
+            if (_stages > 0)
+            {
+                _step = 1f / _stages;
+                stageSwitcher?.Set(0);
+            }
+
             Storage.OnUpdateLink += UpdateLink;
             Init();
         }
@@ -50,6 +71,16 @@ namespace Vortex.Sdk.MiniGamesSystem.MiniGames.Handlers
             _dataTimer = null;
         }
 
+        private void CheckStage()
+        {
+            if (_stages == 0)
+                return;
+            var current = (int)Math.Floor(slider.value / _step);
+            if (current.Equals(_currentStage)) return;
+            _currentStage = current;
+            stageSwitcher.Set(_currentStage);
+        }
+
         private void UpdateLink()
         {
             DeInit();
@@ -63,9 +94,10 @@ namespace Vortex.Sdk.MiniGamesSystem.MiniGames.Handlers
             var passed = (float)_dataTimer.Timer.GetTimePassed().TotalSeconds;
             var duration = (float)_dataTimer.Timer.Duration.TotalSeconds;
             if (duration == 0)
-                slider.value = 1f;
+                slider.value = reverse ? 0f : 1f;
             else
-                slider.value = passed / duration;
+                slider.value = Mathf.Clamp01(reverse ? 1 - passed / duration : passed / duration);
+            CheckStage();
         }
 
 #if UNITY_EDITOR
