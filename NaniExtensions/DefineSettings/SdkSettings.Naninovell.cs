ba6d2a0fa910d4db42487a3f444d9c11af54010b
Юрий Кудрин <rexwolf163@gmail.com>
2026-05-19 20:16:09 +0400

    настройка отключаемости нани-расширения

---
+++ NaniExtensions/DefineSettings/SdkSettings.Naninovell.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Vortex.Sdk.SdkSettingsSystem.Attribute;
+using Vortex.Unity.EditorTools.Attributes;
+
+namespace Vortex.Sdk.SdkSettingsSystem
+{
+    public partial class SdkSettings
+    {
+        [SerializeField, ToggleButton(isSingleButton: true)] [DefineSymbol("USING_NANINOVELL")]
+        private bool naninovellExt = true;
+    }
+}
