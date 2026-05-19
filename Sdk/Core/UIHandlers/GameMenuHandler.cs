4c8e3b810b3feb20d4ecb0dac0e680e04e90781b
Юрий Кудрин <rexwolf163@gmail.com>
2026-05-19 16:47:45 +0400

    доработка GameMenuHandler

--- Sdk/Core/UIHandlers/GameMenuHandler.cs
+++ Sdk/Core/UIHandlers/GameMenuHandler.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using UnityEngine;
+using Vortex.Core.SaveSystem.Bus;
 using Vortex.Sdk.Core.GameCore;
 
 namespace Vortex.Sdk.Core.UIHandlers
@@ -18,6 +20,24 @@ namespace Vortex.Sdk.Core.UIHandlers
         public void SetUnPause()
         {
             GameController.SetPause(false);
+        }
+
+        public void LoadLastGame()
+        {
+            var list = SaveController.GetIndex();
+            if (list.Count == 0)
+                return;
+            var lastGuid = list.Keys.First();
+            var last = list[lastGuid];
+
+            foreach (var (guid, summary) in list)
+            {
+                if (summary.UnixTimestamp <= last.UnixTimestamp) continue;
+                last = summary;
+                lastGuid = guid;
+            }
+
+            SaveController.Load(lastGuid);
         }
 
         public void ExitGame()
