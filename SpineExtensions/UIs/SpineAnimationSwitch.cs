456e17670f8be2ad60fbeafdb282cf7aacae7137
Юрий Кудрин <rexwolf163@gmail.com>
2026-07-06 17:07:52 +0400

    SpineAnimationSwitch

---
+++ SpineExtensions/UIs/SpineAnimationSwitch.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Linq;
+using Sirenix.OdinInspector;
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+using Vortex.Unity.EditorTools.Attributes;
+using Vortex.Unity.UI.StateSwitcher;
+
+namespace Vortex.SpineExtensions.UIs
+{
+    /// <summary>
+    /// Переключение скина для спайн анимации
+    /// </summary>
+    [Serializable]
+    public class SpineAnimationSwitch : StateItem
+    {
+        [SerializeField, ValueSelector("GetListAnimations")]
+        private string animationName;
+
+        [SerializeField, HideIf("@spine==null&&spineAnimation!=null")]
+        private SkeletonGraphic spine;
+
+        [SerializeField, HideIf("@spine!=null")]
+        private SkeletonAnimation spineAnimation;
+
+        [SerializeField, Range(0, 10)] private int channel = 0;
+        [SerializeField] private bool loop = true;
+
+        public override void Set() => SetAnimation();
+
+        public override void DefaultState()
+        {
+            //Нет отключения на default
+        }
+
+        private void SetAnimation()
+        {
+            if (spine != null)
+                spine.AnimationState.SetAnimation(channel, animationName, loop);
+
+            if (spineAnimation != null)
+                spineAnimation.AnimationState.SetAnimation(channel, animationName, loop);
+        }
+
+        private static void ApplySkin(Skeleton skeleton, string skinName)
+        {
+            skeleton.SetSkin(skinName);
+            skeleton.SetSlotsToSetupPose();
+        }
+
+#if UNITY_EDITOR
+
+        public override string DropDownItemName => "Switch Spine Skin";
+        public override string DropDownGroupName => "Animator Control";
+
+        public override StateItem Clone()
+        {
+            return new SpineAnimationSwitch
+            {
+                animationName = animationName,
+                spine = spine,
+                spineAnimation = spineAnimation,
+            };
+        }
+
+        private string[] GetListAnimations()
+        {
+            var data = spine != null
+                ? spine.SkeletonData
+                : spineAnimation != null
+                    ? spineAnimation.SkeletonDataAsset?.GetSkeletonData(true)
+                    : null;
+
+            return data?.Animations.Select(s => s.Name).ToArray() ?? Array.Empty<string>();
+        }
+
+#endif
+    }
+}
