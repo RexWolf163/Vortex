9a9d68c20fdd50490c6eb8bb998f4358be2a1878
Юрий Кудрин <rexwolf163@gmail.com>
2026-09-02 13:24:48 +0400

    фикс убиваемых ссылок нани

--- NaniExtensions/Core/NaniWrapper.cs
+++ NaniExtensions/Core/NaniWrapper.cs
@@ -159,7 +159,6 @@ namespace Vortex.NaniExtensions.Core
 
             ChoiceHandlerManager.ResetService();
 
-            /*
             try
             {
                 ScriptPlayer.Stop();
@@ -170,7 +169,6 @@ namespace Vortex.NaniExtensions.Core
                 if (Settings.Data().DebugMode)
                     Debug.LogException(ex);
             }
-            */
 
             ScriptPlayer.OnPlay += OnScriptEvent;
             ScriptPlayer.OnStop += OnScriptEvent;
